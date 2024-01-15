8f044407dc374e42a00010b1b796fbd8c6fbd005
Bedizz <turkyilmaz.bediz@gmail.com>
2024-01-15 11:35:21 +0100

    fetching data

--- src/components/memebody.jsx
+++ src/components/memebody.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React,{ useEffect, useState } from 'react'
 
 const memebody = () => {
     //const for each function
+    const [meme, setMeme] = useState([])
 
-    // fetch data 
+    // fetching data from the api
+    const getMemeAPI = async () => {
+        try {
+            const res = await fetch('https://api.imgflip.com/get_memes')
+            const data = await res.json()
+            setMeme(data.data.memes)
+            console.log(data.data.memes)
+            
+        } catch (error) {
+            console.log("An error occured during fetching data",error)
+            
+        }
+        
+
+    }
+    // we call the getMemeAPI function in the useEffect
+    useEffect(() => {
+        getMemeAPI()
+    }, [])
+
     // loading stage
     // error stage
     // useeffect part

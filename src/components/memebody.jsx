da4cc915de3a9d284720ec7fad66a33396c5e4b5
Bedizz <turkyilmaz.bediz@gmail.com>
2024-01-15 10:58:53 +0100

    the tasks

---
+++ src/components/memebody.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const memebody = () => {
+    //const for each function
+
+    // fetch data 
+    // loading stage
+    // error stage
+    // useeffect part
+    // --------------------
+    // meme random generator as a function 
+    // --------------------
+    // a function to catch the inputs (2 inputs 1 for top text and 1 for bottom text)
+    // a function to change the imgs (random meme button)
+
+
+
+  return (
+    <div>memebody</div>
+  )
+}
+
+export default memebody

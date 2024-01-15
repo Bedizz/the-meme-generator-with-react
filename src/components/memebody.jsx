0a54031f5e64240c0a4d922f420efce53cfb3662
Tania Boterman <taniaboterman@gmail.com>
2024-01-15 11:40:33 +0100

    meme

--- src/components/memebody.jsx
+++ src/components/memebody.jsx
@@ -2,16 +2,66 @@ import React from 'react'
 
 const memebody = () => {
     //const for each function
+    const [allMemes, setAllMemes] = useState([]);
+    const [memeImage, setMemeImage] = useState({
+      topText: "",
+      bottomText:"",
+      image: "http://i.imgflip.com/1bij.jpg",
+    });
 
     // fetch data 
     // loading stage
     // error stage
     // useeffect part
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // --------------------
     // meme random generator as a function 
-    // --------------------
+  function getMemeImage() {
+    let meme1 = allMemes;
+    meme1 = meme1[Math.floor(Math.random() * meme1.length)];
+    meme1 = meme1.url;
+    return meme1;
+    // console.log(results);
+  }
+
+  // --------------------
     // a function to catch the inputs (2 inputs 1 for top text and 1 for bottom text)
     // a function to change the imgs (random meme button)
+
+  function changeMeme(e) {
+    const { name, value } = e.target;
+    setMemeImage((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setMemeImage((prevState) => ({
+      ...prevState,
+      image: getMemeImage(),
+    }));
+  }
+
 
 
 
